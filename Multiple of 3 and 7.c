bd5fbfe69a5f159f93d9b7be3be2eb6d408b70a6
balasuryashanmugam2007-crypto <balasuryashanmugam2007@gmail.com>
2026-02-17 19:19:11 +0530

    Implement multiple check for 3 and 7 in C program
    This program prompts the user to enter a number and checks if it is a multiple of 3, 7, or both, providing appropriate feedback.

---
+++ Multiple of 3 and 7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 3 == 0 && num % 7 == 0) {
+        printf("Number is a multiple of both 3 and 7.");
+    }
+    else if (num % 3 == 0) {
+        printf("Number is a multiple of 3 only.");
+    }
+    else if (num % 7 == 0) {
+        printf("Number is a multiple of 7 only.");
+    }
+    else {
+        printf("Number is not a multiple of 3 or 7.");
+    }
+
+    return 0;
+}
