628a2b5b6b4817fed8cc807762e0374791953828
balasuryashanmugam2007-crypto <balasuryashanmugam2007@gmail.com>
2026-02-18 10:40:35 +0530

    Add program to check even or odd without modulus

---
+++ Check Even or Odd (Without Using % Operator).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if ((num / 2) * 2 == num) {
+        printf("Even Number");
+    }
+    else {
+        printf("Odd Number");
+    }
+
+    return 0;
+}
