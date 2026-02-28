3b191cd4dc62f0281a8b149af83ebba00aefaa5e
balasuryashanmugam2007-crypto <balasuryashanmugam2007@gmail.com>
2026-02-28 16:13:44 +0530

    Create Valid Triangle Angle Checker.c

---
+++ conditional statement/Valid Triangle Angle Checker.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three angles: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b + c == 180 && a > 0 && b > 0 && c > 0)
+        printf("Valid Triangle\n");
+    else
+        printf("Invalid Triangle\n");
+
+    return 0;
+}
